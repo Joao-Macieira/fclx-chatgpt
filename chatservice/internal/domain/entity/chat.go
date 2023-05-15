233c47dd9872a23683d0b04ed4ef8f4e4eed5d19
João Macieira <jp.macieira@uol.com.br>
2023-05-15 15:49:59 -0300

    feat: create chat model

--- chatservice/internal/domain/entity/chat.go
+++ chatservice/internal/domain/entity/chat.go
@@ -1,1 +1,24 @@
 package entity
+
+// Chat gpt configs, can get in openia documentation
+type ChatConfig struct {
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessage        []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
