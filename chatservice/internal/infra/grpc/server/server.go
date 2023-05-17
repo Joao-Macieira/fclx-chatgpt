8bef9b392ba835a2d2c8518c1474390f32c32c57
João Macieira <jp.macieira@uol.com.br>
2023-05-17 12:25:16 -0300

    feat: create auth interceptor

--- chatservice/internal/infra/grpc/server/server.go
+++ chatservice/internal/infra/grpc/server/server.go
@@ -7,6 +7,10 @@ import (
 	"github.com/joao-macieira/fclx/chatservice/internal/infra/grpc/service"
 	"github.com/joao-macieira/fclx/chatservice/internal/usecases/chatcompletionstream"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type GRPCServer struct {
@@ -30,9 +34,32 @@ func NewGRPCServer(chatCompletionStreamUseCase chatcompletionstream.ChatCompleti
 	}
 }
 
-func (g *GRPCServer) Start() {
-	grpcServer := grpc.NewServer()
+func (g *GRPCServer) AuthInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	ctx := ss.Context()
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Error(codes.Unauthenticated, "metadata is not provided")
+	}
+
+	token := md.Get("authorization")
+	if len(token) == 0 {
+		return status.Error(codes.Unauthenticated, "authorization token is not provided")
+	}
+
+	if token[0] != g.AuthToken {
+		return status.Error(codes.Unauthenticated, "authorization token is invalid")
+	}
+
+	return handler(srv, ss)
+}
+
+func (g *GRPCServer) Start() error {
+	opts := []grpc.ServerOption{
+		grpc.StreamInterceptor(g.AuthInterceptor),
+	}
+	grpcServer := grpc.NewServer(opts...)
 	pb.RegisterChatServiceServer(grpcServer, &g.ChatService)
+	reflection.Register(grpcServer)
 
 	lis, err := net.Listen("tcp", ":"+g.Port)
 
@@ -43,4 +70,6 @@ func (g *GRPCServer) Start() {
 	if err := grpcServer.Serve(lis); err != nil {
 		panic(err.Error())
 	}
+
+	return nil
 }
