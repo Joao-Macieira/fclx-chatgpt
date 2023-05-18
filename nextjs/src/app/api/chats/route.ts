77441110a24dad5b6b96cfb381c834e80f8f7da1
João Macieira <jp.macieira@uol.com.br>
2023-05-18 18:07:54 -0300

    feat: create chat endpoints

--- nextjs/src/app/api/chats/route.ts
+++ nextjs/src/app/api/chats/route.ts
@@ -1,7 +1,41 @@
+import { prisma } from "@/app/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const chats = await prisma.chat.findMany({
+    select: {
+      id: true,
+      messages: {
+        orderBy: {
+          created_at: "asc"
+        },
+        take: 1,
+      }
+    },
+    orderBy: {
+      created_at: "desc"
+    }
+  });
+
+  return NextResponse.json(chats);
+}
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
-  return NextResponse.json({ body });
+  const chatCreated = await prisma.chat.create({
+    data: {
+      messages: {
+        create: {
+          content: body.message
+        }
+      }
+    },
+    select: {
+      id: true,
+      messages: true
+    }
+  });
+
+  return NextResponse.json(chatCreated);
 }
